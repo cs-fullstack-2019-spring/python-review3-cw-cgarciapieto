3f2c16da8d30a8d80d921d7cf2d4899731303959
carlos <cgarciapieto@gmail.com>
2019-01-28 13:03:44 -0600

    graded morning

---
+++ graded_classwork_monday.py
@@ -0,0 +1,33 @@
+def main():
+    problem1()
+
+
+
+# problem2()
+# problem3()
+# problem4()
+
+def problem1():
+    in1to10(11,False)
+
+
+
+def in1to10(n, outside_mode):
+    if outside_mode:
+        return n <= 1 or n >= 10
+    return 1 <= n <= 10
+    print("True")
+    else 
+
+
+
+
+
+
+
+#
+# def problem2():
+# def problem3():
+# def problem4():
+if __name__ == '__main__':
+    main()
